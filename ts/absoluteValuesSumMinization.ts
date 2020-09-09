4aab954f54adbcdd5f12e833b63d429ba3820c04
juniordugue <jrdugue@gmail.com>
2020-09-08 23:56:38 -0400

    added additional js problems in typescript to solve

--- ts/absoluteValuesSumMinization.ts
+++ ts/absoluteValuesSumMinization.ts
@@ -9,7 +9,6 @@ console.log(absoluteValuesSUmMinimization([2, 4, 7, 6])); //4
 console.log(absoluteValuesSUmMinimization([2, 4, 7, 6, 6])); //7
 console.log(absoluteValuesSUmMinimization([2, 4, 7, 6, 6, 8])); //7
 
-
 // Given a sorted array of integers a, find an integer x from a such that the value of
 
 // abs(a[0] - x) + abs(a[1] - x) + ... + abs(a[a.length - 1] - x)
